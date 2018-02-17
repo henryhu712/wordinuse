2ad1af59f54b181dd380abef3971764f2f7eba3f
Double H <h@obserbot.com>
2018-02-18 07:31:04 +0800

    test

--- sites/all/modules/c/home/js/home.js
+++ sites/all/modules/c/home/js/home.js
@@ -1,12 +1,25 @@
 (function($) {
 
+  var words = [];
+  var current_word_index = 0;
+  var current_pos = -1;
+
+  // Display a new word
+  function show_word() {
+    console.log(words);
+    console.log(words[current_word_index]);
+  }
 
   Drupal.behaviors.muread_home = {
     attach: function(context, settings) {
 
 
       var theWord = settings.word_info.the_word;
-      console.log(theWord);
+      //console.log(theWord);
+      words = settings.word_info.words;
+
+      show_word();
+
 
       $('.keyboard-wrap .letter').on('click', function(e) {
         var letter = $(this).data('letter');
