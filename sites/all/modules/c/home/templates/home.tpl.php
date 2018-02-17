0ef58c9c3231f43a13cf533a16308e508f085753
Double H <h@obserbot.com>
2018-02-17 15:54:50 +0800

    word

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,8 +1,10 @@
 
         <div class="row items-container text-center">
-          <a href="/c/all">
-            推荐文章数量： <?php print $number_ding_yue_hao; ?>
-          </a>
+            Complete the word： 
+        <?php foreach ($letters as $letter): ?>
+          <span> <?php print $letter['status'] == 0 ? '-' : $letter['letter']; ?></span>
+        <?php endforeach; ?>
+
         </div>
 
 
