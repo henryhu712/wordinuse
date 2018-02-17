a6201b470b9fb96e9721780b63d4c62b4cee4a12
Ubuntu <ubuntu@ip-172-31-24-127.us-west-2.compute.internal>
2018-02-17 07:19:51 +0000

    home

---
+++ sites/all/modules/c/home/js/home.js
@@ -0,0 +1,21 @@
+(function($) {
+
+
+  Drupal.behaviors.muread_home = {
+    attach: function(context, settings) {
+
+
+
+      // Record hits
+    /*
+      $('.goto_origin').on('click', function() {
+        var nid = $(this).data('nid');
+        $.post('hit', {nid:nid}, function(result) {
+        });
+      });
+      */
+
+    }
+  };
+
+})(jQuery);
